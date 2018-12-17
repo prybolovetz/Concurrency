539bf7a438f81b61772e13ada274098f57062259
Ivan Prybolovetz <ivan.prybo@gmail.com>
2018-12-17 12:02:22 +0300

    first commit

--- Сoncurrent/ViewController.swift
+++ Сoncurrent/ViewController.swift
@@ -9,12 +9,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
+    
+    let urlStr = "https://citydog.by/content/_posts/442X361/5baf9bd2e012d.jpeg?34"
+    var url:NSURL!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        image.layer.cornerRadius = 50.0
+        image.layer.masksToBounds = true
+        url = NSURL(string:urlStr)
+        indicator.startAnimating()
+        indicator.isHidden = true
+        
+        
     }
-
-
+    
+    @IBAction func downloadTapped(_ sender: Any) {
+        
+        //        let data = NSData(contentsOf: url as URL)
+        //        self.imageView.image = UIImage(data: data as! Data)
+        //
+        indicator.isHidden = false
+        
+        URLSession.shared.dataTask(with: (url as URL), completionHandler: {(data, resp, error) -> Void in
+            if (error == nil && data != nil) {
+                //                DispatchQueue.main.async(execute: {
+                //                    self.imageView.image = UIImage(data: data!)
+                //                })
+                
+                OperationQueue.main.addOperation({
+                    self.indicator.isHidden = true
+                    self.image.image = UIImage(data: data!)
+                })
+            }
+        }).resume()
+    }
+    
+    
+    
 }
-
